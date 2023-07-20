03a2a84a5d7d183e59ddbb87e8e5861b91da1017
StephanieLaure <139981164+StephanieLaure@users.noreply.github.com>
2023-07-20 21:24:37 +0200

    Add files via upload

---
+++ images/config_projet.php
@@ -0,0 +1,9 @@
+<?php
+
+define('_DOMAIN_', '.localhost:8888/monprojet/');
+define('ARTICLES_IMAGES_PROJET', "images/");
+define('_ARTICLES_IMAGES_DEFAULT', '/images/automobile_logo.png');
+define('_ADMIN_ITEM_PER_PAGE' , 10);
+define('_APP_EMAIL_', 'queenshopbiz@gmail.com');
+
+?>
